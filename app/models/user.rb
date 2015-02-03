1ded38daa25f32d8db7d3c040cc53b95757f90a7
Daniel Kehoe <daniel@danielkehoe.com>
2015-02-03 15:46:39 +0200

    rails_apps_composer: add README files

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
+  after_create :sign_up_for_mailing_list
 
   def set_default_role
     self.role ||= :user
@@ -10,4 +11,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def sign_up_for_mailing_list
+    MailingListSignupJob.perform_later(self)
+  end
+
+  def subscribe
+    mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
+    result = mailchimp.lists.subscribe({
+      :id => Rails.application.secrets.mailchimp_list_id,
+      :email => {:email => self.email},
+      :double_optin => false,
+      :update_existing => true,
+      :send_welcome => true
+    })
+    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+  end
+
 end
