9a90221168de495912b6a766b9c067b9784109ab
Daniel Kehoe <daniel@danielkehoe.com>
2015-02-03 15:58:33 +0200

    registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
   get 'products/:id', to: 'products#show', :as => :products
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'registrations' }
   resources :users
+  root :to => 'visitors#index'
 end
