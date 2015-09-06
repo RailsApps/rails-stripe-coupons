3ae9856ae0addc7cfdaba7e137277d78af210501
Daniel Kehoe <daniel@danielkehoe.com>
2015-09-06 13:48:25 +0800

    update for Gibbon 2.0

--- app/services/mailing_list_signup_service.rb
+++ app/services/mailing_list_signup_service.rb
@@ -1,13 +1,12 @@
 class MailingListSignupService
 
   def perform(user)
-    mailchimp = Gibbon::API.new(Rails.application.secrets.mailchimp_api_key)
-    result = mailchimp.lists.subscribe({
-      :id => Rails.application.secrets.mailchimp_list_id,
-      :email => {:email => user.email},
-      :double_optin => false,
-      :update_existing => true,
-      :send_welcome => true
+    mailchimp = Gibbon::Request.new(api_key: Rails.application.secrets.mailchimp_api_key)
+    list_id = Rails.application.secrets.mailchimp_list_id
+    result = mailchimp.lists(list_id).members.create(
+      body: {
+        email_address: user.email,
+        status: 'subscribed'
     })
     Rails.logger.info("Subscribed #{user.email} to MailChimp") if result
   end
