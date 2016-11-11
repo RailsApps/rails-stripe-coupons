423af9a0574f798c37af5f9973bf896df11ec420
Daniel Kehoe <daniel@danielkehoe.com>
2016-11-11 14:09:19 +0800

    restore name attribute to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204130343) do
+ActiveRecord::Schema.define(version: 20150203152251) do
 
   create_table "coupons", force: :cascade do |t|
     t.string   "code"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150204130343) do
     t.integer  "role"
     t.integer  "coupon_id"
     t.string   "stripe_token"
+    t.string   "name"
     t.index ["coupon_id"], name: "index_users_on_coupon_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
