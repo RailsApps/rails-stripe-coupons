76d22a019979f927f6ddc1b644c992a256fb7c2d
Daniel Kehoe <daniel@danielkehoe.com>
2016-11-11 14:56:02 +0800

    inherit from ApplicationRecord

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,4 +1,4 @@
-class Coupon < ActiveRecord::Base
+class Coupon < ApplicationRecord
   has_many :users
   validates :code, inclusion: { in: Coupon.pluck('DISTINCT code'),
       message: "value is not a valid coupon code" }
